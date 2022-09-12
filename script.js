02f39bfcdedc14c5f569fb361404a0a7bfa777b2
HannahCunliffe <hannahcunliffe01@googlemail.com>
2022-09-12 16:12:31 +0100

    added button, made base onclick function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//function handling the etch-a-sketch grid generation
 function generateGrid() {
     let gridContainer = document.getElementById("gridContainer");
     for (let i = 0; i < 16; i++) {
@@ -13,4 +14,10 @@ function generateGrid() {
     }
 };
 
+// function called once button is clicked
+function takeUserInput() {
+    let userInput = prompt("Enter a number to determine the grid size i.e 4 would generate a 4x4 grid.");
+    //add logic to restrict input to number only, limit number to no higher than 100
+    return userInput;
+}
 generateGrid();
