39ee3b0b357c75ba0f66a97814a5c9d37926a489
HannahCunliffe <hannahcunliffe01@googlemail.com>
2022-09-12 14:35:46 +0100

    grid hover events completed

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function generateGrid() {
     for (let i = 0; i < 16; i++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("gridSquare");
+        newDiv.addEventListener('mouseover', function handleMouseOver() {
+            newDiv.style.background = 'blue';
+        })
+        newDiv.addEventListener('mouseout', function handleMouseOut() {
+            newDiv.style.background = 'white';
+          });
         gridContainer.appendChild (newDiv);
     }
 };
